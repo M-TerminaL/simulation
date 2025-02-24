04b4730fd8342e5f436b19a2e8082aae3a0b1c15
Alireza Babaei <andromedaa5692@gmail.com>
2025-02-24 21:07:59 +0330

    String : upper method

---
+++ String_methods/upper.py
@@ -0,0 +1,64 @@
+# ---- Upper Method ----
+# Return a Copy of The String Converted To
+# ex: 'alireza'.upper() --> 'ALIREZA'
+string = input('Enter Your String: ')
+
+lower_ref = 'abcdefghijklmnopqrstuvwxyz'
+upper_ref = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# ---- Sequential Way ----
+# ---- Using While ----
+result = ''
+i = 0
+while i < len(string):
+    if string[i] in lower_ref:
+        j = 0
+        while j < len(lower_ref):
+            if string[i] == lower_ref[j]:
+                result = result + upper_ref[j]
+                i = i + 1
+                break
+            else:
+                j = j + 1
+    else:
+        result += string[i]
+        i += 1
+print('using while: ', result)
+# ---- Sequential Way ----
+# ---- Using for ----
+res = ''      
+for index in range(len(string)):
+    if string[index] in lower_ref:
+        for lower_index in range(len(lower_ref)):
+            if string[index] ==  lower_ref[lower_index]:
+                res = res + upper_ref[lower_index]
+    else:
+        res = res + string[index]
+print('using for: ', res)
+                        
+# ---- Functional Way ----
+def personal_upper(str1: str) -> str:
+    lower_ref = 'abcdefghijklmnopqrstuvwxyz'
+    upper_ref = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    upper_result = ''
+    for element in str1:
+        if element in lower_ref:
+            for index in range(len(lower_ref)):
+                if element == lower_ref[index]:
+                    upper_result += upper_ref[index]
+        else:
+            upper_result += element
+    return upper_result
+                
+
+a = personal_upper('aLiReZa13!?')
+print(a)
+
+
+
+
+
+
+
+
+
+        
