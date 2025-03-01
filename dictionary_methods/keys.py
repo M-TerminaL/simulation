ef56c5c9689f24e9581d968f4329ff37d63497b2
Alireza Babaei <andromedaa5692@gmail.com>
2025-03-01 22:19:12 +0330

    dictionary: keys

---
+++ dictionary_methods/keys.py
@@ -0,0 +1,26 @@
+# ---- key Method ----
+# ex: data = {'a': 1, 'b': 2, 'c': 3}
+# ex: data.keys() --> ['a', 'b', 'c']
+# ----
+#------------------------------------------------------
+# ---- Functional Way ----
+
+def keys(dictionary):
+    lst = []
+    for key in dictionary:
+        lst = lst + [key]
+    return lst
+
+a = keys({'a': 1, 'b': 2, 'c': 3, 'd': 4})
+print(a)
+    
+# ---- End Functional Way ----
+#------------------------------------------------------
+
+
+
+
+
+
+
+        
