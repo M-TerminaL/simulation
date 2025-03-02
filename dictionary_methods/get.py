9327986b8abdfe411b3137b9f961d03c9894a0e9
Alireza Babaei <andromedaa5692@gmail.com>
2025-03-02 20:24:59 +0330

    dictionary: get method

---
+++ dictionary_methods/get.py
@@ -0,0 +1,35 @@
+# ---- get Method ----
+# ex: data = {'a': 1, 'b': 2, 'c': 3}
+# ex: data.get('a', default=None) --> 1
+# ----
+#------------------------------------------------------
+# ---- Functional Way ----
+
+def get(dictionary, key, default=None):
+    value = None
+    try:
+        value = dictionary[key]
+    except:
+        if default == None:
+            return
+        else:
+            return default
+    return value
+
+a = get({'a': 1, 'b': 2, 'c': 3}, 'b')
+b = get({'a': 1, 'b': 2, 'c': 3}, 'u', default='not found')
+c = get({'a': 1, 'b': 2, 'c': 3}, 'z')
+
+print(a)
+print(b)
+print(c)
+# ---- End Functional Way ----
+#------------------------------------------------------
+
+
+
+
+
+
+
+        
