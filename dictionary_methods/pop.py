5a910e9524e1f597ebdd81a8419863bd589c7cf8
Alireza Babaei <andromedaa5692@gmail.com>
2025-03-02 20:36:03 +0330

    dictionary: pop method

---
+++ dictionary_methods/pop.py
@@ -0,0 +1,30 @@
+# ---- pop Method ----
+# ex: data = {'a': 1, 'b': 2, 'c': 3}
+# ex: data.pop('a') --> {'b': 2, 'c': 3}
+# ----
+#------------------------------------------------------
+# ---- Functional Way ----
+
+def pop(dictionary, key):
+    try:
+        del dictionary[key]
+    except:
+        return f'key "{key}" not found'
+    return dictionary
+
+a = pop({'a': 1, 'b': 2, 'c': 3}, 'b')
+b = pop({'b': 1, 'b': 2, 'c': 3}, 'z')
+
+print(a)
+print(b)
+
+# ---- End Functional Way ----
+#------------------------------------------------------
+
+
+
+
+
+
+
+        
