bb59684d3f87be74646150b81639619d8da92586
Alireza Babaei <andromedaa5692@gmail.com>
2025-03-03 20:10:13 +0330

    tuple: index method

---
+++ tuple_methods/index.py
@@ -0,0 +1,35 @@
+# ---- index Method ----
+# ex: data = (1, 2, 3, 1, 4, 1)
+# ex: data.index(1, start=1, stop=len(data)) --> 3
+# ----
+#------------------------------------------------------
+# ---- Functional Way ----
+
+def index(set_, value, start=0, stop=None):
+    idx = None
+    if stop == None:
+        stop = len(set_)
+    for index in range(start, stop):
+        if set_[index] == value:
+            idx = index
+            break
+    return idx
+
+a = index(('a', 'b', 'c', 'b', 'd'), 'b', start=2, stop=5)
+b = index(('a', 'b', 'c', 'b', 'd'), 'z')
+c = index(('a', 'b', 'c', 'b', 'd'), 'b')
+
+print(a)
+print(b)
+print(c)
+
+# ---- End Functional Way ----
+#------------------------------------------------------
+
+
+
+
+
+
+
+        
