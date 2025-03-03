54990e86f24520e808495ab3f5e85ce39e38a291
Alireza Babaei <andromedaa5692@gmail.com>
2025-03-03 19:56:44 +0330

    tuple: count method

---
+++ tuple_methods/count.py
@@ -0,0 +1,31 @@
+# ---- setdefault Method ----
+# ex: data = (1, 2, 3, 1, 4, 1)
+# ex: data.count(1) --> 3
+# ----
+#------------------------------------------------------
+# ---- Functional Way ----
+
+def count(set_, value):
+    counter = 0
+    for elem in set_:
+        if elem == value:
+            counter += 1
+    return counter
+        
+
+a = count((1, 2, 3, 4, 1, 2, 11, 1, 6), 1)
+b = count((1, 2, 3, 4, 1, 2, 11, 1, 6), 66)
+
+print(a)
+print(b)
+
+# ---- End Functional Way ----
+#------------------------------------------------------
+
+
+
+
+
+
+
+        
