9a03de0419e3ae33a6717c8561f8f75fa29727ab
Alireza Babaei <andromedaa5692@gmail.com>
2025-03-05 20:59:44 +0330

    class base : Dict Class

---
+++ class_base/dict.py
@@ -0,0 +1,55 @@
+# --- Class Dict ---
+
+class DictModifier:
+    def __init__(self, dictionary):
+        self.dictionary = dictionary
+
+    def __repr__(self):
+        return str(self.dictionary)
+
+    def keys(self):
+        result = []
+        for key in self.dictionary:
+            result = result + [key]
+        return result
+
+    def values(self):
+        result = []
+        for key in self.dictionary:
+            result = result + [self.dictionary[key]]
+        return result
+
+    def get(self, key, default=None):
+        result = 'Not Found'
+        try:
+           result = self.dictionary[key]
+        except:
+            if default is None:
+                return result
+            else:
+                result = default
+                return result
+        return result
+
+    def items(self):
+        result = []
+        for key in self.dictionary:
+            result = result + [(key, self.dictionary[key])]
+        return result
+
+    def pop(self, key):
+        try:
+            value = self.dictionary[key]
+            del self.dictionary[key]
+        except:
+            value = f'key: " {key} " not found.'
+        return value
+
+    def setdefault(self, key, default=None):
+        if key in self.dictionary:
+            return self.dictionary[key]
+        else:
+            self.dictionary[key] = default
+            
+
+    
